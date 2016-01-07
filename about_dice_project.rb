7e627239db96d7796b884ebc88c1aa8026c6faac
Katarzyna Starachowicz <kstarachowicz@gmail.com>
2016-01-07 22:53:51 +0100

    classes without message passing

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -2,9 +2,19 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # Implement a DiceSet Class here:
 #
-# class DiceSet
-#   code ...
-# end
+class DiceSet
+  attr_reader :values
+
+  def initialize
+  	@values = []
+  end
+
+  def roll(a)
+  	@values = []
+  	a.times { @values << (rand(6) + 1) }
+  end
+
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
